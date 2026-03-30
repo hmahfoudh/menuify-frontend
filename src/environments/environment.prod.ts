439f9530fb4f3550218b762b2d5155f7307fbf2e
houssemmh <houssemmahfoudh94@gmail.com>
2026-03-30 15:58:50 +0100

    fix: env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend.edsolutions.com.tn',
-  defaultLang: 'en',
-  cloudHost: 'https://edquiz.edsolutions.com.tn',
-  socket:'https://backend.edsolutions.com.tn/ws',
-  serverImageUrl: 'https://backend.edsolutions.com.tn',
-  recaptcha: {
-    siteKey: '6LdO3_IqAAAAALBqGqjcOQbbFSrDHD3p_M7TRrG7',
-  },
+  apiUrl: 'https://backend.menuify.tn',
+  defaultLang: 'en'
 };
